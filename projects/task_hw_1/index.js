bbfab9e3417a9d78b1cbb46c3791c56dd8ae7651
IvanSyAlex <protonunet@gmail.com>
2020-12-18 23:09:49 +0500

    Выполненое ДЗ1 18.12.20

--- projects/task_hw_1/index.js
+++ projects/task_hw_1/index.js
@@ -12,7 +12,9 @@
 
  Другими словами: функция должна возвращать в неизменном виде то, что поступает ей на вход
  */
-function returnFirstArgument() {}
+function returnFirstArgument(Argument) {
+  return Argument;
+}
 
 /*
  Задание 2:
@@ -28,7 +30,9 @@ function returnFirstArgument() {}
  Пример:
    sumWithDefaults(10) вернет 110
  */
-function sumWithDefaults(a, b) {}
+function sumWithDefaults(a, b = 100) {
+  return a + b;
+}
 
 /*
  Задание 3:
@@ -38,7 +42,9 @@ function sumWithDefaults(a, b) {}
  Пример:
    returnFnResult(() => 'привет') вернет 'привет'
  */
-function returnFnResult(fn) {}
+function returnFnResult(fn) {
+  return fn();
+}
 
 /*
  Задание 4:
@@ -53,7 +59,16 @@ function returnFnResult(fn) {}
    console.log(f()); // выведет 12
    console.log(f()); // выведет 13
  */
-function returnCounter(number) {}
+function returnCounter(number = 0) {
+  return function () {
+    return ++number;
+  };
+}
+const resFunction = returnCounter(10);
+
+console.log(resFunction());
+console.log(resFunction());
+console.log(resFunction());
 
 /*
  Задание 5 *:
@@ -64,7 +79,14 @@ function returnCounter(number) {}
  Пример:
    returnArgumentsArray(1, 2, 3) вернет [1, 2, 3]
  */
-function returnArgumentsArray() {}
+function returnArgumentsArray() {
+  const arr = new Array(arguments.length);
+  for (let i = 0; i < arguments.length; i++) {
+    arr[i] = arguments[i];
+  }
+  return arr;
+}
+console.log(returnArgumentsArray(1, 2, 3, 9));
 
 /*
  Задание 6 *:
@@ -81,7 +103,23 @@ function returnArgumentsArray() {}
 
    console.log(newSum()) выведет 6
  */
-function bindFunction(fn) {}
+function sum(...args) {
+  let sum = 0;
+  for (let i = 0; i < args.length; i++) {
+    if (args[i]) {
+      sum += args[i];
+    }
+  }
+  return sum;
+}
+
+function bindFunction(fn, ...args) {
+  return () => fn(...args);
+}
+
+const newSum = bindFunction(sum, 2, 4);
+
+console.log(newSum());
 
 export {
   returnFirstArgument,
