4c382b0cfca13711ee11a4675d06a2a9e0b7fd57
IvanSyAlex <protonunet@gmail.com>
2020-12-27 13:28:45 +0500

    tsak_2 added

--- projects/exceptions/index.js
+++ projects/exceptions/index.js
@@ -49,7 +49,22 @@ isAllTrue([1, 2, 3], (n) => n < 10);
    isSomeTrue([1, 2, 30, 4, 5], n => n > 20) // вернет true
    isSomeTrue([1, 2, 3, 4, 5], n => n > 20) // вернет false
  */
-function isSomeTrue(array, fn) {}
+function isSomeTrue(array, fn) {
+  let bolean = false;
+  if (!Array.isArray(array) || !array.length) {
+    throw new Error('empty array');
+  }
+  if (!(typeof fn === 'function')) {
+    throw new Error('fn is not a function');
+  }
+  for (let i = 0; i < array.length; i++) {
+    if (fn(array[i])) {
+      bolean = true;
+    }
+  }
+  return bolean;
+}
+isSomeTrue([1, 2, 30, 4, 5], (n) => n > 10);
 
 /*
  Задание 3:
